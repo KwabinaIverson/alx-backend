99e62e26fffd4859bc0e515c63c567142b3774e8
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-04-10 03:25:06 +0000

    Basic Flask setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
-from flask_babel import Babel
+#!/usr/bin/env python3
+
+from flask import Flask
+from routes.route_0 import app_routes
 
 app = Flask(__name__)
 
-app.route('/')
-def index():
-    return render_template('index.html')
+app.register_blueprint(app_routes)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
